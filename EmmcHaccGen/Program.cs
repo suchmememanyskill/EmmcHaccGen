ff1c6584a071fa23751258868afb2be23066cce7
Such Meme, Many Skill <owen.vanrijn@gmail.com>
2020-02-05 00:01:34 +0100

    Remove lots of useless comments

--- EmmcHaccGen/Program.cs
+++ EmmcHaccGen/Program.cs
@@ -42,59 +42,6 @@ namespace EmmcHaccGen
 
             return entry;
         }
-        /*
-        Byte[] ReadFile(ref IFileSystem filesystem, string path)
-        {
-            Byte[] tempByte;
-            IFile tempFile;
-            long readlength, writelength;
-
-            filesystem.OpenFile(out tempFile, path, OpenMode.Read);
-            tempFile.GetSize(out readlength);
-            tempByte = new Byte[readlength];
-            tempFile.Read(out writelength, 0, tempByte, ReadOption.None);
-            tempFile.Dispose();
-
-            Console.WriteLine($"File: {path}, ReadSize: {readlength}, OutSize: {writelength}");
-
-            return tempByte;
-        }
-        void Pad(ref List<Byte> bytelist, long padamount)
-        {
-            for (int i = 0; i < padamount; i++)
-            {
-                bytelist.Add(0);
-            }
-        }
-        void AddBctBytes(ref List<Byte> bytelist, ref IFileSystem filesystem)
-        {
-            Byte[] tempByte;
-
-            tempByte = ReadFile(ref filesystem, "/nx/bct");
-            tempByte[0x210] = 0x77;
-
-            bytelist.AddRange(tempByte.ToList());
-            Pad(ref bytelist, (0x4000 - tempByte.Length));
-        }
-        void AddPkg1Bytes(ref List<Byte> bytelist, ref IFileSystem filesystem)
-        {
-            Byte[] tempByte;
-
-            tempByte = ReadFile(ref filesystem, "/nx/package1");
-
-            bytelist.AddRange(tempByte.ToList());
-            Pad(ref bytelist, (0x40000 - tempByte.Length));
-        }
-        void AddPkg2Bytes(ref List<Byte> bytelist, ref IFileSystem filesystem, long extrapaddingsize)
-        {
-            Byte[] tempByte;
-
-            tempByte = ReadFile(ref filesystem, "/nx/package2");
-
-            bytelist.AddRange(tempByte.ToList());
-            Pad(ref bytelist, (0x800000 - tempByte.Length - extrapaddingsize));
-        }
-        */
         Dictionary<string, List<ncaList>> SortNca(List<ncaList> list, List<string> forbiddenlist)
         {
             Dictionary<string, List<ncaList>> NcaDict = new Dictionary<string, List<ncaList>>();
@@ -121,7 +68,6 @@ namespace EmmcHaccGen
                 var ncalistsorted = ncalist.Value.OrderBy(i => i.type != NcaContentType.Meta).ToList();
                 NcaDict[ncalist.Key] = ncalistsorted;
             }
-            //            Console.WriteLine($"{Convert.ToInt32("3A3B3C", 16)}");
 
             foreach (var item in NcaDict.OrderBy(x => Convert.ToInt64(x.Key, 16)))
                 SortedNcaDict.Add(item.Key, item.Value);
@@ -141,12 +87,6 @@ namespace EmmcHaccGen
 
             List<ncaList> ncalist = new List<ncaList>();
             keyset = ExternalKeyReader.ReadKeyFile("prod.keys");
-            /*
-            List<Byte> boot0 = new List<byte>();
-            List<Byte> boot1 = new List<byte>();
-            List<Byte> bcpkg2_1 = new List<byte>();
-            List<Byte> bcpkg2_3 = new List<byte>();
-            */
             bis boot0 = new bis(0x180000);
             bis boot1 = new bis(0x80000);
             bis bcpkg2_1 = new bis(0x800000);
@@ -208,20 +148,12 @@ namespace EmmcHaccGen
             bcpkg2_3.Write(SafeExtractor.pkg2);
             bcpkg2_3.DumpToFile("bcpkg2_3.testnew");
 
-            Dictionary<string, List<ncaList>> test = SortNca(ncalist, forbiddenlist);
+            Dictionary<string, List<ncaList>> SortedNcaPairDict = SortNca(ncalist, forbiddenlist);
             List<imen> imenlist = new List<imen>();
 
-            foreach (KeyValuePair<string, List<ncaList>> hi in test)
+            foreach (var pair in SortedNcaPairDict)
             {
-                /*
-                Console.WriteLine($"Key: {hi.Key}");
-
-                foreach (ncaList hi2 in hi.Value)
-                {
-                    Console.WriteLine($"^- {hi2.filename} {hi2.type}");
-                }
-                */
-                imen newimen = new imen(keyset, hi.Value);
+                imen newimen = new imen(pair.Value);
                 newimen.Gen();
                 imenlist.Add(newimen);
             }
@@ -229,7 +161,7 @@ namespace EmmcHaccGen
 
             imkv final = new imkv(imenlist);
             final.Build();
-            final.DumpToFile("imkvdb.arc");
+            //final.DumpToFile("data.arc");
 
             if (File.Exists("120.save"))
                 File.Delete("120.save");
@@ -246,127 +178,7 @@ namespace EmmcHaccGen
                 }
                 save.Commit(keyset);
             }
-
-            /*
-            imen testimen = new imen(keyset, test["0100000000000816"]);
-            testimen.Gen();
-            //imen test2 = new imen(keyset, test.ElementAt(0).Value);
-            //test2.GetCnmt($"9.1.0\\{test.ElementAt(0).Value[0].filename}");
-            //test2.Gen();
-
-
-
-            
-            foreach (var entry in ncalist)
-            {
-                Console.WriteLine($"File: {entry.filename}, TitleID: {entry.titleid}, Type: {entry.type}");
-            }
-
-            ncaList Normal = ncalist.Find(x => x.titleid == "0100000000000819" && x.type == NcaContentType.Data);
-            ncaList Safe = ncalist.Find(x => x.titleid == "010000000000081A" && x.type == NcaContentType.Data);
-
-            IStorage inNormal = new LocalStorage($"9.1.0\\{Normal.filename}", FileAccess.Read);
-            var ncaNormal = new Nca(keyset, inNormal);
-            IFileSystem filesystemNormal = ncaNormal.OpenFileSystem(NcaSectionType.Data, IntegrityCheckLevel.None);
-
-            IStorage inSafe = new LocalStorage($"9.1.0\\{Safe.filename}", FileAccess.Read);
-            var ncaSafe = new Nca(keyset, inSafe);
-            IFileSystem filesystemSafe = ncaSafe.OpenFileSystem(NcaSectionType.Data, IntegrityCheckLevel.None);
-
-            foreach (var temp in filesystemSafe.EnumerateEntries())
-            {
-                Console.WriteLine(temp.FullPath);
-            }
-
-            AddBctBytes(ref boot0, ref filesystemNormal);
-            AddBctBytes(ref boot0, ref filesystemSafe);
-            AddBctBytes(ref boot0, ref filesystemNormal);
-            AddBctBytes(ref boot0, ref filesystemSafe);
-            Pad(ref boot0, 0xF0000);
-            AddPkg1Bytes(ref boot0, ref filesystemNormal);
-            AddPkg1Bytes(ref boot0, ref filesystemNormal);
-
-            AddPkg1Bytes(ref boot1, ref filesystemSafe);
-            AddPkg1Bytes(ref boot1, ref filesystemSafe);
-
-            Pad(ref bcpkg2_1, 0x4000);
-            AddPkg2Bytes(ref bcpkg2_1, ref filesystemNormal, 0x4000);
-
-            Pad(ref bcpkg2_3, 0x40000);
-            AddPkg2Bytes(ref bcpkg2_3, ref filesystemSafe, 0x40000);
-
-            //IFileSystem filesystemCode = ncaNormal.OpenFileSystem(NcaSectionType.Code, IntegrityCheckLevel.None);
-            //filesystemCode.
-            //holy fuck https://github.com/Thealexbarney/LibHac/blob/d08e6b060c913a8a08df4b546a3cb290dc07851f/src/LibHac/SwitchFs.cs#L142
-
-            inNormal.Dispose();
-            inSafe.Dispose();
-
-            if (File.Exists("boot0.test"))
-                File.Delete("boot0.test");
-
-            if (File.Exists("boot1.test"))
-                File.Delete("boot1.test");
-
-            if (File.Exists("bcpkg2_1.test"))
-                File.Delete("bcpkg2_1.test");
-
-            if (File.Exists("bcpkg2_3.test"))
-                File.Delete("bcpkg2_3.test");
-
-            using (Stream file = File.OpenWrite("boot0.test"))
-            {
-                file.Write(boot0.ToArray(), 0, boot0.Count);
-            }
-
-            using (Stream file = File.OpenWrite("boot1.test"))
-            {
-                file.Write(boot1.ToArray(), 0, boot1.Count);
-            }
-
-            using (Stream file = File.OpenWrite("bcpkg2_1.test"))
-            {
-                file.Write(bcpkg2_1.ToArray(), 0, bcpkg2_1.Count);
-            }
-
-            using (Stream file = File.OpenWrite("bcpkg2_3.test"))
-            {
-                file.Write(bcpkg2_3.ToArray(), 0, bcpkg2_3.Count);
-            }
-
-            Dictionary<string, List<ncaList>> test = SortNca(ncalist);
-
-            foreach(KeyValuePair<string, List<ncaList>> hi in test)
-            {
-                Console.WriteLine($"Key: {hi.Key}");
-
-                foreach(ncaList hi2 in hi.Value)
-                {
-                    Console.WriteLine($"^- {hi2.filename} {hi2.type}");
-                }
-
-                using (IStorage tempncastorage = new LocalStorage($"9.1.0\\{hi.Value.Find(x => x.type == NcaContentType.Meta).filename}", FileAccess.Read))
-                {
-                    var tempNca = new Nca(keyset, tempncastorage);
-                    using (IFileSystem tempncafilesystem = tempNca.OpenFileSystem(NcaSectionType.Data, IntegrityCheckLevel.ErrorOnInvalid))
-                    {
-                        foreach (var temp in tempncafilesystem.EnumerateEntries())
-                        {
-                            Console.WriteLine(temp.FullPath);
-                        }
-                    }
-                }
-
-
-
-
-
-                //tempncafilesystem.Dispose();
-                //tempncastorage.Dispose();
-
-            }
-
-            */
+            Console.WriteLine($"Wrote save with an imvkdb size of 0x{final.bytes.Count:X4}");
 
             Console.ReadKey();
         }
--- EmmcHaccGen/imen.cs
+++ EmmcHaccGen/imen.cs
@@ -25,12 +25,12 @@ namespace EmmcHaccGen
 
         public void Build()
         {
-            bytes.Add(0x49);
+            bytes.Add(0x49); // Spells out IMKV
             bytes.Add(0x4D);
             bytes.Add(0x4B);
             bytes.Add(0x56);
 
-            bytes.Add(0x0);
+            bytes.Add(0x0); // Padding
             bytes.Add(0x0);
             bytes.Add(0x0);
             bytes.Add(0x0);
@@ -59,22 +59,17 @@ namespace EmmcHaccGen
     {
         public List<Byte> bytes;
         public List<Byte> key, value;
-        //private Cnmt title;
-        private Keyset keyset;
-        //private byte[] cnmt_raw;
         private List<ncaList> pair;
-        private int offset = 0x20;
         private CnmtRawParser cnmtRawParser;
 
 
         public imen() { }
 
-        public imen(Keyset keyset, List<ncaList> pair)
+        public imen(List<ncaList> pair)
         {
             bytes = new List<Byte>();
             key = new List<Byte>();
             value = new List<Byte>();
-            this.keyset = keyset;
             this.pair = pair;
         }
 
@@ -85,19 +80,8 @@ namespace EmmcHaccGen
 
             GenKey();
             GenValue();
-            
-            //Console.WriteLine($"{pair[0].titleid.ToLower()}: {BitConverter.ToString(key.ToArray()).Replace("-", "").ToLower()} {BitConverter.ToString(value.ToArray()).Replace("-", "").ToLower()}");
-            //Console.WriteLine($"{BitConverter.ToUInt64(test.raw_title_id)} {pair[0].cnmt.TitleId}");
-            //'0000020000000000dab17bb26feb93ad1523d55a7b59f6e9000e00000000000000ae6772e51a6a19e9d0a45a2ca2e8450068110000000100'
 
-            /*
-            Structure of to_content_info()
-            self.raw.content_id == filename without .nca (hash)
-            self.raw.size == nca.header.size
-
-            */
-
-            bytes.Add(0x49);
+            bytes.Add(0x49); // Spells out IMEN
             bytes.Add(0x4D);
             bytes.Add(0x45);
             bytes.Add(0x4E);
@@ -107,7 +91,7 @@ namespace EmmcHaccGen
             bytes.AddRange(key);
             bytes.AddRange(value);
 
-            Console.WriteLine($"{pair[0].titleid.ToLower()}: {BitConverter.ToString(bytes.ToArray()).Replace("-", "").ToLower()}");
+            //Console.WriteLine($"{pair[0].titleid.ToLower()}: {BitConverter.ToString(bytes.ToArray()).Replace("-", "").ToLower()}");
         }
         private void GenKey()
         {
@@ -123,24 +107,6 @@ namespace EmmcHaccGen
         }
         private void GenValue()
         {
-            /*
-            UInt16 raw_ext_header_size = BitConverter.ToUInt16(pair[0].raw_cnmt, 0xE);
-            value.AddRange(BitConverter.GetBytes(raw_ext_header_size));
-            value.AddRange(BitConverter.GetBytes((UInt16)(pair[0].cnmt.ContentEntryCount + 1)));
-            value.AddRange(BitConverter.GetBytes((UInt16)pair[0].cnmt.MetaEntryCount));
-            value.Add(pair[0].raw_cnmt[0x14]);
-            value.Add(0);
-
-            if (raw_ext_header_size > 0)
-            {
-                byte[] ext_header_size = new byte[raw_ext_header_size];
-                for (offset = 0x20; offset < raw_ext_header_size + 0x20; offset++)
-                    ext_header_size[offset - 0x20] = pair[0].raw_cnmt[offset];
-
-
-                value.AddRange(ext_header_size.ToList());
-            }
-            */
             value.AddRange(cnmtRawParser.raw_ext_header_size);
             value.AddRange(BitConverter.GetBytes((UInt16)(cnmtRawParser.content_count + 1)));
             value.AddRange(BitConverter.GetBytes((UInt16)(cnmtRawParser.content_meta_count)));
@@ -164,19 +130,7 @@ namespace EmmcHaccGen
             if (cnmtRawParser.ext_data != null)
             {
                 value.AddRange(cnmtRawParser.ext_data);
-                //value.AddRange(BitConverter.GetBytes((uint)cnmtRawParser.ext_data.Length));
             }
-                
-
-            
-            
-
-            // Add Content info
-            // Add content_meta info
-
-            //title.ContentEntries[0].
-
-
         }
         private void AddContentValue(int number)
         {
@@ -185,14 +139,9 @@ namespace EmmcHaccGen
             {
                 value.AddRange(Enumerable.Range(0, pair[number].filename.Length - 4).Where(x => x % 2 == 0).Select(x => Convert.ToByte(pair[number].filename.Substring(x, 2), 16)));
                 //value.AddRange(BitConverter.GetBytes(Convert.ToUInt16(pair[number].filename.Substring(0, pair[number].filename.Length - 4), 16)));
-                //value.AddRange(BitConverter.GetBytes(0x000e00000000));
-
-                
 
                 value.AddRange(BitConverter.GetBytes((UInt16)pair[number].size));
 
-                //value.Add(0xe);
-                //value.Add(0);
                 value.Add(0);
                 value.Add(0);
                 value.Add(0);
@@ -203,16 +152,6 @@ namespace EmmcHaccGen
             }
             else
             {
-                //todo: cnmt needs rawparser
-                /*
-                value.AddRange(BitConverter.GetBytes(BitConverter.ToUInt16(pair[0].raw_cnmt, offset + 0x30)));
-                value.AddRange(BitConverter.GetBytes(BitConverter.ToUInt16(pair[0].raw_cnmt, offset + 0x32)));
-                value.AddRange(BitConverter.GetBytes(BitConverter.ToUInt16(pair[0].raw_cnmt, offset + 0x34)));
-
-                value.Add((Byte)pair[0].cnmt.ContentEntries[0].Type);
-                value.Add(0);
-                */
-
                 foreach(var temp in cnmtRawParser.content)
                 {
                     value.AddRange(temp.GetRawRecord());
