ca10a50cd2d7efc00ed9d76315b665bf1ac3e7d0
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2020-06-25 22:05:06 +0200

    Take version data from meta NCA instead of data NCA

--- EmmcHaccGen/nca/VersionExtractor.cs
+++ EmmcHaccGen/nca/VersionExtractor.cs
@@ -10,44 +10,26 @@ namespace EmmcHaccGen.nca
 {
     class VersionExtractor
     {
-        private byte[] rawVersion;
-        public string platform, version;
+        public NcaFile versionMetaNca;
+        private TitleVersion ExtractedVersion
+        {
+            get
+            {
+                return versionMetaNca.cnmt.TitleVersion;
+            }
+        }
+        public string Version
+        {
+            get
+            {
+                return $"{ExtractedVersion.Major}.{ExtractedVersion.Minor}.{ExtractedVersion.Patch}";
+            }
+        }
 
         public VersionExtractor() { }
         public VersionExtractor(NcaFile nca)
         {
-            try
-            {
-                this.OpenVersionNca(nca);
-                this.Parse();
-            }
-            catch
-            {
-                platform = "NX";
-                version = "Unkn";
-                Console.WriteLine("[Warning] Version extraction failed. Version is unknown");
-            }
-        }
-        private void OpenVersionNca(NcaFile nca)
-        {
-            using IFileSystem fs = nca.data.OpenFileSystem(NcaSectionType.Data, IntegrityCheckLevel.ErrorOnInvalid);
-
-            fs.OpenFile(out IFile version, new U8Span("/file"), OpenMode.Read).ThrowIfFailure();
-            version.GetSize(out long readSize);
-            rawVersion = new byte[readSize];
-            version.Read(out long temp, 0, rawVersion);
-        }
-        private void Parse()
-        {
-            platform = Encoding.UTF8.GetString(ReadFromOffset(0x20, 0x8)).Trim('\0').ToLower();
-            version = Encoding.UTF8.GetString(ReadFromOffset(0x18, 0x68)).Trim('\0').ToLower();
-        }
-        private byte[] ReadFromOffset(uint amount, uint offset)
-        {
-            byte[] temp = new byte[amount];
-            for (int i = 0; i < amount; i++)
-                temp[i] = rawVersion[i + offset];
-            return temp;
+            versionMetaNca = nca;
         }
     }
 }
